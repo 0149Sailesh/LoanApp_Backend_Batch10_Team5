e8808c086807824cbc6319272a337ab4b891f90f
Abhinav Anand <10.abhinav.anand.01@gmail.com>
2023-08-21 10:57:45 +0530

    loan apply done

---
+++ Backend/LoanAPI/Controllers/ApplyLoanController.cs
@@ -0,0 +1,67 @@
+﻿using LoanAPI.DTOs;
+using LoanAPI.Entites;
+using LoanAPI.Service;
+using LOANAPI.Entites;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LoanAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplyLoanController : ControllerBase
+    {
+        private readonly LoanDbContext _dbconteact;
+        public ApplyLoanController(LoanDbContext dbconteact)
+        {
+            _dbconteact = dbconteact;
+        }
+        //Endpoints
+        [HttpPost, Route("ApplyforLoan")]
+        public IActionResult Add(ApplyLoanDTO loanDTO)
+        {
+            try
+            {
+                var loan_details = _dbconteact.LCMEntity.FirstOrDefault(o => o.Loan_Type.ToLower() == loanDTO.Item_Category.ToLower());
+                Random rnd = new Random();
+                if (loan_details != null)
+                {
+                    EmployeeCardDetailsEntity emp = new EmployeeCardDetailsEntity()
+                    {
+                        Card_Id = "Ca"+rnd.Next(1,9)+rnd.Next(1,9)+rnd.Next(1,9),
+                        Employee_Id = loanDTO.Employee_Id,
+                        Loan_Id=loan_details.Loan_Id,
+                        Card_Issue_Date=DateTime.Now
+                    };
+                    //_dbconteact.SaveChanges();
+                    var item_details = _dbconteact.IMEntity.FirstOrDefault(o => o.Item_Category.ToLower() == loanDTO.Item_Category.ToLower());
+                    if (item_details != null)
+                    {
+                        DateTime today = DateTime.Now;
+                        DateTime return_date = today.AddDays(365 * loan_details.Duration);
+                        EmployeeIssueDetailsEntity emp1 = new EmployeeIssueDetailsEntity()
+                        {
+                            Issue_Id = "Is" + rnd.Next(1, 9) + rnd.Next(1, 9) + rnd.Next(1,9),
+                            Employee_Id = loanDTO.Employee_Id,
+                            Item_Id = item_details.Item_Id,
+                            Issue_Date = DateTime.Now,
+                            Return_Date = DateTime.Now
+                        };
+                        _dbconteact.ECDEntity.Add(emp);
+                        _dbconteact.EIDEntity.Add(emp1);
+                        _dbconteact.SaveChanges();
+                        return StatusCode(200, new JsonResult("Employee Card and Issue Details added!"));
+                    }
+                }
+                
+                return StatusCode(400, new JsonResult("Loan cannot be issued!"));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Backend/LoanAPI/DTOs/ApplyLoanDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace LoanAPI.DTOs
+{
+    public class ApplyLoanDTO
+    {
+        public string Employee_Id { get; set; }
+        public string Item_Category { get; set; }
+        public string Item_Description { get; set; }
+        public string Item_Value{ get; set; }
+        public string Item_Make { get; set; }
+        public DateTime Date_of_Application { get; set; }
+        //public DateTime Date_of_Joining { get; set; }
+    }
+}
